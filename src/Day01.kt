a1e4c4165e17ad5a8a86ca361eb70e80deb1882c
Artur Skowronski <me@arturskowronski.com>
2023-12-01 07:35:36 +0100

    Solution 1 - No refactor, I need to take daughter to PreSchool...

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,85 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val result = it
+                    .split("").mapNotNull { it.toIntOrNull() }
+
+            println(result.first() * 10 + result.last())
+            result.first() * 10 + result.last()
+        }
+    }
+
+    fun preprocessHead(it: String): String {
+        var watchdog = true
+        var inputString = it
+        while (watchdog) {
+            var head = "";
+            var tail = "";
+            for (i in 0..<inputString.length+1) {
+                head = inputString.take(i)
+                        .replace("one", "1")
+                        .replace("two", "2")
+                        .replace("three", "3")
+                        .replace("four", "4")
+                        .replace("five", "5")
+                        .replace("six", "6")
+                        .replace("seven", "7")
+                        .replace("eight", "8")
+                        .replace("nine", "9")
+                        .replace("zero", "0")
+                tail = inputString.drop(i)
+                if (inputString.length != head.length + tail.length) break
+            }
+            if (inputString.length == head.length + tail.length) watchdog = false
+            else {
+                inputString = head + tail
+            }
+        }
+        return inputString;
+    }
+
+    fun preprocessTail(it: String): String {
+        var watchdog = true
+        var inputString = it
+        while (watchdog) {
+            var head = "";
+            var tail = "";
+            for (i in 0..<inputString.length+1) {
+                tail = inputString.takeLast(i)
+                        .replace("one", "1")
+                        .replace("two", "2")
+                        .replace("three", "3")
+                        .replace("four", "4")
+                        .replace("five", "5")
+                        .replace("six", "6")
+                        .replace("seven", "7")
+                        .replace("eight", "8")
+                        .replace("nine", "9")
+                        .replace("zero", "0")
+                head = inputString.dropLast(i)
+                if (inputString.length != head.length + tail.length) break
+            }
+            if (inputString.length == head.length + tail.length) watchdog = false
+            else {
+                inputString = head+tail
+            }
+        }
+        return inputString;
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+         return input.sumOf {
+            val resultHead = preprocessHead(it)
+                    .split("").mapNotNull { it.toIntOrNull() }
+
+             val resultTail = preprocessTail(it)
+                     .split("").mapNotNull { it.toIntOrNull() }
+
+             resultHead.first() * 10 + resultTail.last()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    part1(input).println()
+    // part1(input).println()
     part2(input).println()
 }
